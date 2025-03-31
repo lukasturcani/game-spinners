c547c99bdf2d50718cee3a49b2f2de6bbc39e040
Lukas Turcani <lukasturcani93@gmail.com>
2025-03-31 12:29:14 +0100

    WIP

--- crates/snake/src/lib.rs
+++ crates/snake/src/lib.rs
@@ -15,9 +15,10 @@ pub fn app() -> App {
 struct Score(u16);
 
 #[derive(Debug, Resource)]
-struct BoxSize {
+struct BoxOutline {
     width: u16,
     height: u16,
+    border: u16,
 }
 
 #[derive(Debug, Component)]
@@ -45,26 +46,29 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let box_size = BoxSize {
-        width: 10,
-        height: 10,
+    let box_outline = BoxOutline {
+        width: 60,
+        height: 30,
+        border: 1,
     };
-
     commands.spawn((
         Camera2d,
         OrthographicProjection {
-            scale: 0.01,
+            scale: 0.05,
             ..OrthographicProjection::default_2d()
         },
         Transform::from_xyz(0.0, 0.0, 0.0),
     ));
     commands.spawn((
-        Mesh2d(meshes.add(Circle::new(1.))),
-        MeshMaterial2d(materials.add(Color::from(RED))),
-        Transform::from_xyz(0.0, 0.0, 0.0),
+        Mesh2d(meshes.add(Rectangle::new(
+            box_outline.border as f32,
+            (box_outline.height + box_outline.border * 2) as f32,
+        ))),
+        MeshMaterial2d(materials.add(Color::from(GRAY))),
+        Transform::from_xyz(-(box_outline.width as f32) / 2., 0.0, 0.0),
     ));
     commands.insert_resource(Score(0));
-    commands.insert_resource(box_size);
+    commands.insert_resource(box_outline);
 }
 
 fn spawn_snake(
@@ -83,19 +87,18 @@ fn spawn_snake(
     ));
 }
 
-fn move_snakes(mut snakes: Query<(&mut Transform, &mut Snake)>, box_size: Res<BoxSize>) {
-    return;
+fn move_snakes(mut snakes: Query<(&mut Transform, &mut Snake)>, box_outline: Res<BoxOutline>) {
     for (mut transform, mut snake) in snakes.iter_mut() {
         match snake.direction {
             Direction::Up => {
                 if snake.head_position.1 == 0 {
-                    snake.head_position.1 = box_size.height - 1;
+                    snake.head_position.1 = box_outline.height - 1;
                 } else {
                     snake.head_position.1 -= 1;
                 }
             }
             Direction::Down => {
-                if snake.head_position.1 == box_size.height - 1 {
+                if snake.head_position.1 == box_outline.height - 1 {
                     snake.head_position.1 = 0;
                 } else {
                     snake.head_position.1 += 1;
@@ -103,13 +106,13 @@ fn move_snakes(mut snakes: Query<(&mut Transform, &mut Snake)>, box_size: Res<Bo
             }
             Direction::Left => {
                 if snake.head_position.0 == 0 {
-                    snake.head_position.0 = box_size.width - 1;
+                    snake.head_position.0 = box_outline.width - 1;
                 } else {
                     snake.head_position.0 -= 1;
                 }
             }
             Direction::Right => {
-                if snake.head_position.0 == box_size.width - 1 {
+                if snake.head_position.0 == box_outline.width - 1 {
                     snake.head_position.0 = 0;
                 } else {
                     snake.head_position.0 += 1;
